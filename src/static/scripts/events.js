89353b7db770be3352aa6869d370d33254354efe
Ning Ning Sun <nsun200@live.com>
2019-10-14 21:27:45 -0400

    Add highlighting

--- src/static/scripts/events.js
+++ src/static/scripts/events.js
@@ -1,7 +1,36 @@
+let isAcross = true;
+let squares = [];
+
+function setDir(dir, row, col) {
+    // dir = true => across, down otherwise
+    isAcross = dir
+    for (let i = 0; i < squares.length; i++ ) {
+        for (let j = 0; j < squares[i].length; j++) {
+            squares[i][j].classList.remove('selected-group');
+        }
+    }
+
+    for (let i = 0; i < squares.length; i++) {
+        let square = squares[row][i];
+        if (!square.classList.contains('black-square')) {
+            if (dir) {
+                squares[row][i].classList.add('selected-group');
+            }
+        }
+
+        square = squares[i][col]
+        if (!square.classList.contains('black-square')) {
+            if (!dir) {
+                squares[i][col].classList.add('selected-group');
+            }
+        }
+    }
+    
+}
+
 $(document).ready(function() {
     let socket = io.connect('http://' + document.domain + ':' + location.port);
 
-    let squares = [];
 
     let button = document.getElementById('check-button');
     button.addEventListener('click', function() {
@@ -54,17 +83,51 @@ $(document).ready(function() {
 
                     letterDiv.addEventListener('input', function (e) {
                         socket.emit('update', {'row': letterDiv.getAttribute('data-row'), 'col': letterDiv.getAttribute('data-col'), 'letter': e.data});
+                        if (e.data !== '') {
+                            if (isAcross) {
+                                if (j >= parsedData.length - 1) {
+                                    return;
+                                }
+                                for (let k = j + 1; k <= parsedData.length - 1; k++) {
+                                    if (row[k].classList.contains('black-square') || row[k].childNodes[1].classList.contains('correct')) {
+                                        continue;
+                                    }
+                                    else {
+                                        row[k].childNodes[1].focus();
+                                        return;
+                                    }
+                                }
+                            }
+                            else {
+                                if (i >= parsedData.length - 1) {
+                                    return;
+                                }
+                                for (let k = i + 1; k <= parsedData.length - 1; k++) {
+                                    if (squares[k][j].classList.contains('black-square') || squares[k][j].childNodes[1].classList.contains('correct')) {
+                                        continue;
+                                    }
+                                    else {
+                                        squares[k][j].childNodes[1].focus();
+                                        return;
+                                    }
+                                }
+                            }
+                        }
                     });
 
                     letterDiv.addEventListener('keydown', function(e) {
                         let key = e.keyCode;
+                        if (key == 8) {
+                            letterDiv.innerHTML = '';
+                            socket.emit('update', {'row': letterDiv.getAttribute('data-row'), 'col': letterDiv.getAttribute('data-col'), 'letter': ''});
+                        }
                         if (key === 37) {
                             // left arrow
+                            setDir(true, i, j);
                             if (j <= 0) {
                                 return;
                             }
                             for (let k = j - 1; k >= 0; k--) {
-                                console.log(k)
                                 if (row[k].classList.contains('black-square') || row[k].childNodes[1].classList.contains('correct')) {
                                     continue;
                                 }
@@ -76,6 +139,7 @@ $(document).ready(function() {
                         }
                         else if (key === 38) {
                             // up arrow
+                            setDir(false, i, j);
                             if (i <= 0) {
                                 return;
                             }
@@ -91,11 +155,11 @@ $(document).ready(function() {
                         }
                         else if (key === 39) {
                             // right arrow
+                            setDir(true, i, j);
                             if (j >= parsedData.length - 1) {
                                 return;
                             }
                             for (let k = j + 1; k <= parsedData.length - 1; k++) {
-                                console.log(k)
                                 if (row[k].classList.contains('black-square') || row[k].childNodes[1].classList.contains('correct')) {
                                     continue;
                                 }
@@ -107,6 +171,7 @@ $(document).ready(function() {
                         }
                         else if (key === 40) {
                             // down arrow
+                            setDir(false, i, j);
                             if (i >= parsedData.length - 1) {
                                 return;
                             }
@@ -123,6 +188,7 @@ $(document).ready(function() {
                     })
 
                     letterDiv.addEventListener('focus', function () {
+                        setDir(isAcross, i, j);
                         td.classList.add('selected');
                     });
 
