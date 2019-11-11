a25928bb32181df0741ccb05eb9a99558e2a29e5
Ning Ning Sun <nsun200@live.com>
2019-11-10 20:17:05 -0500

    refactor js code

--- src/static/scripts/events.js
+++ src/static/scripts/events.js
@@ -3,6 +3,7 @@ let squares = [];
 let numRows = 15;
 let numCols = 15;
 
+
 function setDir(dir, row, col) {
     // dir = true => across, down otherwise
     isAcross = dir
@@ -30,9 +31,217 @@ function setDir(dir, row, col) {
     }
 }
 
+
+function getClueId(row, col) {
+    if (isAcross) {
+        let i = col;
+        let square;
+        while (square.childNodes[0].innerHTML === '') {
+            square = squares[row][i];
+            i--;
+        }
+    }
+    else {
+        let i = row;
+        while (square.childNodes[0].innerHTML === '') {
+            square = squares[i][col];
+            i--;
+        }
+    }
+    return square.childNodes[0].innerHTML;
+}
+
+
+function addKeydownListener(letterDiv, i, j, socket) {
+    letterDiv.addEventListener('keydown', function(e) {
+        let key = e.keyCode;
+        if (key == 8) {
+            // backspace
+            if (letterDiv.innerHTML !== '') {
+                letterDiv.innerHTML = '';
+                socket.emit('update', {'row': letterDiv.getAttribute('data-row'), 'col': letterDiv.getAttribute('data-col'), 'letter': ''});
+            }
+            else {
+                if (isAcross) {
+                    setDir(true, i, j);
+                    if (j <= 0) {
+                        return;
+                    }
+                    for (let k = j - 1; k >= 0; k--) {
+                        if (squares[i][k].classList.contains('black-square') || squares[i][k].childNodes[1].classList.contains('correct')) {
+                            continue;
+                        }
+                        else {
+                            squares[i][k].childNodes[1].focus();
+                            return;
+                        }
+                    }
+                }
+                else {
+                    setDir(false, i, j);
+                    if (i <= 0) {
+                        return;
+                    }
+                    for (let k = i - 1; k >= 0; k--) {
+                        if (squares[k][j].classList.contains('black-square') || squares[k][j].childNodes[1].classList.contains('correct')) {
+                            continue;
+                        }
+                        else {
+                            squares[k][j].childNodes[1].focus();
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        if (key === 37) {
+            // left arrow
+            let wasAcross = isAcross;
+            setDir(true, i, j);
+            if (!wasAcross) {
+                return;
+            }
+            if (j <= 0) {
+                return;
+            }
+            for (let k = j - 1; k >= 0; k--) {
+                if (squares[i][k].classList.contains('black-square') || squares[i][k].childNodes[1].classList.contains('correct')) {
+                    continue;
+                }
+                else {
+                    squares[i][k].childNodes[1].focus();
+                    return;
+                }
+            }
+        }
+        else if (key === 38) {
+            // up arrow
+            let wasAcross = isAcross;
+            setDir(false, i, j);
+            if (wasAcross) {
+                return;
+            }
+            if (i <= 0) {
+                return;
+            }
+            for (let k = i - 1; k >= 0; k--) {
+                if (squares[k][j].classList.contains('black-square') || squares[k][j].childNodes[1].classList.contains('correct')) {
+                    continue;
+                }
+                else {
+                    squares[k][j].childNodes[1].focus();
+                    return;
+                }
+            }
+        }
+        else if (key === 39) {
+            // right arrow
+            let wasAcross = isAcross;
+            setDir(true, i, j);
+            if (!wasAcross) {
+                return;
+            }
+            if (j >= numCols - 1) {
+                return;
+            }
+            for (let k = j + 1; k <= numCols - 1; k++) {
+                if (squares[i][k].classList.contains('black-square') || squares[i][k].childNodes[1].classList.contains('correct')) {
+                    continue;
+                }
+                else {
+                    squares[i][k].childNodes[1].focus();
+                    return;
+                }
+            }
+        }
+        else if (key === 40) {
+            // down arrow
+            let wasAcross = isAcross;
+            setDir(false, i, j);
+            if (wasAcross) {
+                return;
+            }
+            if (i >= numRows - 1) {
+                return;
+            }
+            for (let k = i + 1; k <= numRows - 1; k++) {
+                if (squares[k][j].classList.contains('black-square') || squares[k][j].childNodes[1].classList.contains('correct')) {
+                    continue;
+                }
+                else {
+                    squares[k][j].childNodes[1].focus();
+                    return;
+                }
+            }
+        }
+    });
+}
+
+
+function addDoubleClickListener(letterDiv, i, j) {
+    letterDiv.addEventListener('dblclick', function(e) {
+        setDir(!isAcross, i, j);
+    });
+}
+
+
+function addInputListener(letterDiv, i, j, socket) {
+    letterDiv.addEventListener('input', function (e) {
+        socket.emit('update', {'row': letterDiv.getAttribute('data-row'), 'col': letterDiv.getAttribute('data-col'), 'letter': e.data});
+        if (e.data !== '') {
+            if (isAcross) {
+                if (j >= numCols - 1) {
+                    return;
+                }
+                for (let k = j + 1; k <= numCols - 1; k++) {
+                    if (squares[i][k].classList.contains('black-square') || squares[i][k].childNodes[1].classList.contains('correct')) {
+                        continue;
+                    }
+                    else {
+                        squares[i][k].childNodes[1].focus();
+                        return;
+                    }
+                }
+            }
+            else {
+                if (i >= numRows - 1) {
+                    return;
+                }
+                for (let k = i + 1; k <= numRows - 1; k++) {
+                    if (squares[k][j].classList.contains('black-square') || squares[k][j].childNodes[1].classList.contains('correct')) {
+                        continue;
+                    }
+                    else {
+                        squares[k][j].childNodes[1].focus();
+                        return;
+                    }
+                }
+            }
+        }
+    });
+}
+
+
+function addEventListeners(letterDiv, td, i, j, socket) {
+    addInputListener(letterDiv, i, j, socket);
+
+    addKeydownListener(letterDiv, i, j, socket);
+
+    addDoubleClickListener(letterDiv, i, j);
+
+    letterDiv.addEventListener('focus', function () {
+        setDir(isAcross, i, j);
+        td.classList.add('selected');
+    });
+
+    letterDiv.addEventListener('blur', function () {
+        td.classList.remove('selected');
+    });
+}
+
+
 $(document).ready(function() {
     let socket = io.connect('http://' + document.domain + ':' + location.port);
-
 
     let button = document.getElementById('check-button');
     button.addEventListener('click', function() {
@@ -86,177 +295,11 @@ $(document).ready(function() {
                     letterDiv.setAttribute('data-col', j);
                     letterDiv.setAttribute('contenteditable', true);
 
-                    letterDiv.addEventListener('input', function (e) {
-                        socket.emit('update', {'row': letterDiv.getAttribute('data-row'), 'col': letterDiv.getAttribute('data-col'), 'letter': e.data});
-                        if (e.data !== '') {
-                            if (isAcross) {
-                                if (j >= numCols - 1) {
-                                    return;
-                                }
-                                for (let k = j + 1; k <= numCols - 1; k++) {
-                                    if (row[k].classList.contains('black-square') || row[k].childNodes[1].classList.contains('correct')) {
-                                        continue;
-                                    }
-                                    else {
-                                        row[k].childNodes[1].focus();
-                                        return;
-                                    }
-                                }
-                            }
-                            else {
-                                if (i >= numRows - 1) {
-                                    return;
-                                }
-                                for (let k = i + 1; k <= numRows - 1; k++) {
-                                    if (squares[k][j].classList.contains('black-square') || squares[k][j].childNodes[1].classList.contains('correct')) {
-                                        continue;
-                                    }
-                                    else {
-                                        squares[k][j].childNodes[1].focus();
-                                        return;
-                                    }
-                                }
-                            }
-                        }
-                    });
-
-                    letterDiv.addEventListener('keydown', function(e) {
-                        let key = e.keyCode;
-                        if (key == 8) {
-                            if (letterDiv.innerHTML !== '') {
-                                letterDiv.innerHTML = '';
-                                socket.emit('update', {'row': letterDiv.getAttribute('data-row'), 'col': letterDiv.getAttribute('data-col'), 'letter': ''});
-                            }
-                            else {
-                                if (isAcross) {
-                                    setDir(true, i, j);
-                                    if (j <= 0) {
-                                        return;
-                                    }
-                                    for (let k = j - 1; k >= 0; k--) {
-                                        if (row[k].classList.contains('black-square') || row[k].childNodes[1].classList.contains('correct')) {
-                                            continue;
-                                        }
-                                        else {
-                                            row[k].childNodes[1].focus();
-                                            return;
-                                        }
-                                    }
-                                }
-                                else {
-                                    setDir(false, i, j);
-                                    if (i <= 0) {
-                                        return;
-                                    }
-                                    for (let k = i - 1; k >= 0; k--) {
-                                        if (squares[k][j].classList.contains('black-square') || squares[k][j].childNodes[1].classList.contains('correct')) {
-                                            continue;
-                                        }
-                                        else {
-                                            squares[k][j].childNodes[1].focus();
-                                            return;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        if (key === 37) {
-                            // left arrow
-                            let wasAcross = isAcross;
-                            setDir(true, i, j);
-                            if (!wasAcross) {
-                                return;
-                            }
-                            if (j <= 0) {
-                                return;
-                            }
-                            for (let k = j - 1; k >= 0; k--) {
-                                if (row[k].classList.contains('black-square') || row[k].childNodes[1].classList.contains('correct')) {
-                                    continue;
-                                }
-                                else {
-                                    row[k].childNodes[1].focus();
-                                    return;
-                                }
-                            }
-                        }
-                        else if (key === 38) {
-                            // up arrow
-                            let wasAcross = isAcross;
-                            setDir(false, i, j);
-                            if (wasAcross) {
-                                return;
-                            }
-                            if (i <= 0) {
-                                return;
-                            }
-                            for (let k = i - 1; k >= 0; k--) {
-                                if (squares[k][j].classList.contains('black-square') || squares[k][j].childNodes[1].classList.contains('correct')) {
-                                    continue;
-                                }
-                                else {
-                                    squares[k][j].childNodes[1].focus();
-                                    return;
-                                }
-                            }
-                        }
-                        else if (key === 39) {
-                            // right arrow
-                            let wasAcross = isAcross;
-                            setDir(true, i, j);
-                            if (!wasAcross) {
-                                return;
-                            }
-                            if (j >= numCols - 1) {
-                                return;
-                            }
-                            for (let k = j + 1; k <= numCols - 1; k++) {
-                                if (row[k].classList.contains('black-square') || row[k].childNodes[1].classList.contains('correct')) {
-                                    continue;
-                                }
-                                else {
-                                    row[k].childNodes[1].focus();
-                                    return;
-                                }
-                            }
-                        }
-                        else if (key === 40) {
-                            // down arrow
-                            let wasAcross = isAcross;
-                            setDir(false, i, j);
-                            if (wasAcross) {
-                                return;
-                            }
-                            if (i >= numRows - 1) {
-                                return;
-                            }
-                            for (let k = i + 1; k <= numRows - 1; k++) {
-                                if (squares[k][j].classList.contains('black-square') || squares[k][j].childNodes[1].classList.contains('correct')) {
-                                    continue;
-                                }
-                                else {
-                                    squares[k][j].childNodes[1].focus();
-                                    return;
-                                }
-                            }
-                        }
-                    })
-
-                    letterDiv.addEventListener('focus', function () {
-                        setDir(isAcross, i, j);
-                        td.classList.add('selected');
-                    });
-
-                    letterDiv.addEventListener('blur', function () {
-                        td.classList.remove('selected');
-                    });
+                    addEventListeners(letterDiv, td, i, j, socket);
 
                     td.appendChild(numDiv);
                     td.appendChild(letterDiv);
                 }
-
-
-
                 row.push(td);
                 tr.appendChild(td);
             }
